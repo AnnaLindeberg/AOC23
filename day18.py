0b65f0dd6cdac5ce47d6f590e41a439ba2f9332d
AnnaLindeberg <anna@familjenlindeberg.com>
2023-12-21 11:40:45 +0100

    unfinished part 2 day 18

--- day18.py
+++ day18.py
@@ -1,89 +1,86 @@
 # Day 18 of Advent of Code 2023: Lavaduct Lagoon
 # https://adventofcode.com/2023/day/18
+from collections import namedtuple, deque
 import numpy as np
-from collections import deque
-from bisect import insort
 
-def BFSinteriorCount(startPos, trench):
-    queue = deque()
-    queue.append(startPos)
-    visited = set()
-    while len(queue) > 0:
-        element = queue.popleft()
-        visited.add(element)
-        element = np.array(element)
-        for shift in [np.array([1,0]), np.array([-1,0]), np.array([0,-1]), np.array([0,1])]:
-            neighbor = tuple(element + shift)
-            if neighbor in trench:
-                continue
-            elif neighbor not in visited:
-                queue.append(neighbor)
-    return len(visited)
+Segment = namedtuple('Segment', ['x', 'yStart', 'yEnd'])
 
-def iterativeInteriorCount(trench, yMin, yMax):
-    interiorInPrevRow = set()
-    res = 0
-    for y in range(yMin, yMax + 1):
-        isInside = False
-        interiorInThisRow = set()
-        consecutiveTrenchLen = 0
-        for x in range(trench[y][0], trench[y][-1] + 1):
-            if x in trench[y]:
-                consecutiveTrenchLen += 1
+def areaBetween(stackSeg, nonStackSeg, peakStackX):
+    assert stackSeg.yEnd == nonStackSeg.yStart
+    if (nonStackSeg.yStart < nonStackSeg.yEnd and nonStackSeg.yEnd < stackSeg.yStart) or (nonStackSeg.yStart > nonStackSeg.yEnd and nonStackSeg.yEnd > stackSeg.yStart):
+        area = (abs(stackSeg.x - nonStackSeg.x))*abs(nonStackSeg.yEnd - nonStackSeg.yStart)
+        stackSeg = Segment(stackSeg.x, stackSeg.yStart, nonStackSeg.yEnd)
+        nonStackSeg = None
+    elif abs(peakStackX - nonStackSeg.x) >= abs(stackSeg.x - nonStackSeg.x):
+        area = (abs(stackSeg.x - nonStackSeg.x))*abs(nonStackSeg.yEnd - nonStackSeg.yStart)
+        if stackSeg.yStart == nonStackSeg.yEnd:
+            nonStackSeg = None
+        else:
+            nonStackSeg = Segment(stackSeg.x, stackSeg.yStart, nonStackSeg.yEnd)
+        stackSeg = None
+    else:
+        area = (abs(stackSeg.x - nonStackSeg.x) - 1)*(abs(stackSeg.yEnd - stackSeg.yStart)-1) + abs(peakStackX - nonStackSeg.x)
+        if stackSeg.yStart == nonStackSeg.yEnd:
+            nonStackSeg = None
+        else:
+            nonStackSeg = Segment(stackSeg.x, stackSeg.yStart, nonStackSeg.yEnd)
+        stackSeg = None
+    return area, stackSeg, nonStackSeg
+
+def runningCount(instructions):
+    borderCount, interiorCount = 0, 0
+    upDownStack = deque()
+    stackDir = None
+    xPos, yPos = 0, 0
+    for dir, dist in instructions:
+        borderCount += dist
+        if dir == 'R':
+            xPos += dist
+        elif dir == 'L':
+            xPos -= dist
+        elif stackDir is None or dir == stackDir:
+            stackDir = dir
+            tmp = yPos
+            if stackDir == 'D':
+                yPos -= dist
             else:
-                # first check if you just arrived at a non-trench-spot
-                if consecutiveTrenchLen == 1:
-                    # Have passed past wall, in our out of interior
-                    isInside = not isInside
-                    consecutiveTrenchLen = 0
-                elif consecutiveTrenchLen > 1:
-                    # determined by whether the pos above is or isnt inside
-                    isInside = x in interiorInPrevRow
-                    consecutiveTrenchLen = 0
-                
-                # then deal with this position itself
-                if isInside:
-                    interiorInThisRow.add(x)
-        res += len(interiorInThisRow)
-        interiorInPrevRow = interiorInThisRow
-    return res
-
-def reevaluateExtremes(pos, xMin, xMax, yMin, yMax):
-    return min(xMin, pos[0]), max(xMax, pos[0]), min(yMin, pos[1]), max(yMax, pos[1])
-
-def printGrid(trench, xMin, xMax, yMin, yMax):
-    for y in range(yMin, yMax + 1):
-        for x in range(xMin, xMax + 1):
-            if x in trench[y]:
-                print('#', end='')
+                yPos += dist
+            upDownStack.append(Segment(xPos, tmp, yPos))
+        else:
+            tmp = yPos
+            if stackDir == 'D':
+                yPos -= dist
             else:
-                print('.', end='')
-        print('') 
+                yPos += dist
+            thisSeg = Segment(xPos, tmp, yPos)
+            stackSeg = upDownStack.pop()
+            while thisSeg is not None and stackSeg is not None:
+                if len(upDownStack) > 0:
+                    xSneakPeak = upDownStack[-1].x
+                else:
+                    xSneakPeak = stackSeg.x
+                area, stackSeg, thisSeg = areaBetween(stackSeg, thisSeg, xSneakPeak)
+                interiorCount += area
+                if stackSeg is None and len(upDownStack) > 0:
+                    stackSeg = upDownStack.pop()
+
+            if thisSeg is None and stackSeg is None:
+                stackDir = None
+            elif stackSeg is None:
+                upDownStack.append(thisSeg)
+                stackDir = dir
+    return borderCount + interiorCount 
+
 
 def main():
-    directions = {'R': np.array([1,0]), 'L': np.array([-1,0]), 'U': np.array([0,-1]), 'D': np.array([0,1])}
-    currentPos = np.array([0,0])
-    trench = {}
-    xMin, xMax, yMin, yMax = 0, 0, 0, 0
+    instr1 = []
     with open("small_input18.txt") as file:
         for row in file:
             dir, steps, hex = row.strip().split()
             steps = int(steps)
-            dir = directions[dir]
-            for _ in range(steps):
-                if currentPos[1] in trench:
-                    insort(trench[currentPos[1]], currentPos[0])
-                else:
-                    trench[currentPos[1]] = [currentPos[0]]
-                
-                xMin, xMax, yMin, yMax = reevaluateExtremes(currentPos, xMin, xMax, yMin, yMax)
-                # trench.add(tuple(currentPos))
-                currentPos += dir
-    
-    
-    # printGrid(trench, xMin, xMax, yMin, yMax)
-    interior = iterativeInteriorCount(trench, yMin, yMax) #BFSinteriorCount((1,1), trench)
-    ans1 = interior + sum(map(len, trench.values()))
+            instr1.append((dir, steps))
+            
+    ans1 = runningCount(instr1)
     print(f"Task 1: {ans1}\nTask 2: {0}")
 
 
